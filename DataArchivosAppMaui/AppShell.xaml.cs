7c64ccc73796b996e1191c7d1ca79f23c4e8e54b
laboratorio <laboratorio@UPOAULA10610>
2024-11-22 07:44:36 -0500

    Agregar archivos de proyecto.

---
+++ DataArchivosAppMaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DataArchivosAppMaui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DataArchivosAppMaui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DataArchivosAppMaui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
